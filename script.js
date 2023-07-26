8846aaa92bb075ea6b775cfa8a40a289476f1146
marocuisine <140588437+marocuisine@users.noreply.github.com>
2023-07-26 09:41:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,35 @@ links.forEach((link) => {
 const navbar = document.getElementById('nav');
 let prevScrollPos = window.pageYOffset;
 let isNavbarHidden = false;
+let isNavbarOpen = false;
+const navToggleButton = document.getElementById('icons');
+navToggleButton.addEventListener('click', () => {
+  if (isNavbarOpen) {
+    closeNavbar();
+  } else {
+    openNavbar();
+  }
+});
 
+// Gestionnaire de clic sur le document pour fermer la barre de navigation en dehors de celle-ci
+document.addEventListener('click', (event) => {
+  const targetElement = event.target;
+  if (!navbar.contains(targetElement) && targetElement !== navToggleButton) {
+    closeNavbar();
+  }
+});
+
+// Fonction pour ouvrir la barre de navigation
+function openNavbar() {
+  navbar.style.transform = 'translateY(0)';
+  isNavbarOpen = true;
+}
+
+// Fonction pour fermer la barre de navigation
+function closeNavbar() {
+  navbar.style.transform = 'translateY(-100%)';
+  isNavbarOpen = false;
+}
 function handleScroll() {
   const currentScrollPos = window.pageYOffset;
 
